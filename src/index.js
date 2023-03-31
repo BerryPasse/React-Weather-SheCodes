73ba2e533799c4ab055f6cb8158ce32c12b97eeb
BPasse <104333390+BerryPasse@users.noreply.github.com>
2023-03-31 14:21:16 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
-import "./App.css";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
